d8c1cd16ce2983ab91ef8939e749c3d27e3908ca
yannick grenzinger <yannick.grenzinger@payfit.com>
2025-07-20 21:04:22 +0200

    added sandbox directory with first example

--- test/run.js
+++
@@ -1,8 +0,0 @@
-import { OrgChart } from "../src/d3-org-chart.js";
-import data from "./orgchart-example.json";
-
-// Create and render the org chart
-new OrgChart()
-    .container('#chart-container')
-    .data(data)
-    .render();
